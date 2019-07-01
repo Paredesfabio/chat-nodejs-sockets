334f08abf99aeb8d60d6ecf9eb37aa1587f4da92
Fabio Paredes <fabio.paredes@gmail.com>
2019-07-01 12:29:05 -0500

    Primer Commit

---
+++ server/classes/usuarios.js
@@ -0,0 +1,44 @@
+class Usuarios {
+
+    constructor(){
+        this.personas = [];
+    }
+
+    agregarPersona(id, nombre, sala){
+        let persona = { id, nombre, sala };
+        this.personas.push(persona);
+
+        return this.personas;
+    }
+
+    getPersona(id){
+        let persona = this.personas.filter( persona => persona.id === id )[0];
+
+        return persona;
+    }
+
+    getPersonas(){
+        return this.personas;
+    }
+
+    getPersonasPorSala(sala){
+        let personasEnSala = this.personas.filter( persona => persona.sala === sala );
+        return personasEnSala;
+    }
+
+    borrarPersona(id){
+
+        let personaBorrada = this.getPersona(id);
+
+        // creo un nuevo arreglo y excluyo la persona con el id enviado
+        this.personas = this.personas.filter(persona => persona.id != id);
+
+        return personaBorrada;
+
+    }
+
+}
+
+module.exports = { 
+    Usuarios 
+};
---
+++ server/sockets/socket.js
@@ -0,0 +1,51 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const usuarios = new Usuarios();
+
+const { crearMensaje } = require('../utilidades/utilidades');
+
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (data, callback) => {
+
+        if( !data.usuario.nombre || !data.usuario.sala ){
+            let err = {
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
+            };            
+            return callback(err);
+        }
+
+        // unir al usuario a una sala de chat
+        client.join(data.usuario.sala);
+
+        usuarios.agregarPersona( client.id, data.usuario.nombre, data.usuario.sala ); 
+
+        // listar las personas de esa sala de chat
+        client.broadcast.to(data.usuario.sala).emit('listaPersonas', usuarios.getPersonasPorSala(data.usuario.sala));
+
+        callback( usuarios.getPersonasPorSala(data.usuario.sala) );
+
+    });
+ 
+    client.on('crearMensaje', (data) => {
+        let persona = usuarios.getPersona( client.id );
+        let mensaje = crearMensaje( persona.nombre, data.mensaje );
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+    });
+
+    client.on('disconnect', () => {
+        let personaBorrada = usuarios.borrarPersona( client.id );
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} abandono el chat`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersonas', usuarios.getPersonasPorSala(personaBorrada.sala));
+    });
+
+    // Mensaje Privado
+    client.on('mensajePrivado', data => {
+        let persona = usuarios.getPersona(client.id);
+        // Al broadcast se le agrega el to.( id del usuario )
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+    });    
+
+});
